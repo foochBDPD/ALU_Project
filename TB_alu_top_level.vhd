d5298d761e00683909d03323ba5962e08d2357dc
fooch <m.p.palladino@gmail.com>
2023-11-04 22:44:17 -0400

    [FIXING_REPO] - Dropped Files back in to latest

--- TB_alu_top_level.vhd
+++ TB_alu_top_level.vhd
@@ -1,10 +1,18 @@
+--Mike Palladino
+--ALU Project
+--
+--Date: 8-27-23
+--------------------
+
+
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use std.textio.all;
-use work.pkg_testbench.all;
-use ieee.numeric_std.all;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.NUMERIC_STD.ALL;
 USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+USE WORK.PKG_TESTBENCH.ALL;
 USE IEEE.STD_LOGIC_MISC.ALL;
+USE STD.TEXTIO.ALL;
+
 
 entity alu_testbench is
 	generic (
@@ -16,13 +24,12 @@ entity alu_testbench is
 		);
 	port (
         tb_clk                 : out      std_logic;                      -- Clock signal
-        tb_reg_a               : out      std_logic_vector(7 downto 0);   -- Input register A to alu_top_level
-        tb_reg_b               : out      std_logic_vector(7 downto 0);   -- Input register B to alu_top_level
-		tb_alu_output          : inout       std_logic_vector(7 downto 0);   -- Output of "alu_top_level" for test bench to print to screen
+       
+		tb_alu_output          : inout    std_logic_vector(7 downto 0);   -- Output of "alu_top_level" for test bench to print to screen
 		
-		tb_instr_ram_data_in   : out      std_logic_vector(G_RAM_DATA_SIZE - 1 downto 0) := (others => '0');
-		tb_instr_ram_addr      : out      unsigned(g_RAM_ADDR_SIZE - 1 downto 0) := (others => '0');
-		tb_dp_instr_ram_we     : out      std_logic := '0'
+		tb_instr_ram_data_in   : out      std_logic_vector(g_RAM_DATA_SIZE - 1 downto 0) := (others => '0');
+		tb_instr_ram_addr      : out      unsigned        (g_RAM_ADDR_SIZE - 1 downto 0) := (others => '0');
+		tb_dp_instr_ram_we     : out      std_logic                                      :=            '0'
 		);
 end entity alu_testbench;
 
@@ -44,11 +51,8 @@ architecture TB of alu_testbench is
   
   --time constants
   constant sim_time_limit    : time    := 1000 ns;  -- Simulation time limit (adjust as needed)
-  constant clock_period      : time    := 10 ns;  -- Desired clock period (adjust as needed)
-  
-  
-  --   n/a
-    
+  constant clock_period      : time    := 5 ns;  -- Desired clock period (adjust as needed)
+       
   ---------------------------------------
   -- Components
   ---------------------------------------
@@ -59,11 +63,15 @@ architecture TB of alu_testbench is
   -- Signals
   ---------------------------------------
 	
-	signal testname      : string(1 to 64)              := (others => ' ');
-	signal clock_internal  : std_logic;
-	signal instr_command : std_logic_vector(7 downto 0) := (others => '0');
-	
+	signal testname        : string(1 to 64)              := (others => ' ');
+	signal clock_internal  : std_logic := '0';
+	signal instr_command   : std_logic_vector(7 downto 0) := (others => '0');
 
+
+    signal stb_instr_ram_data_in  : std_logic_vector(g_RAM_DATA_SIZE - 1 downto 0) := (others => '0');
+	signal stb_instr_ram_addr     : unsigned        (g_RAM_ADDR_SIZE - 1 downto 0) := (others => '0');
+	signal stb_dp_instr_ram_we    : std_logic                                      :=            '0';
+	signal address_tracker        : natural 		:= 31;
   ---------------------------------------
   -- Procedures
   ---------------------------------------
@@ -76,11 +84,8 @@ begin
 		--Instantiate the Unit under Test "alu_top_level.vhd" 
 		UUT : entity work.ALU_1 
 			port map(
-				clk           => tb_clk,										--clock driving from "alu_top_level_tb.vhd" to UUT(alu_top_level.vhd)
-				reg_a         => tb_reg_a,										--reg_a data from "alu_command_file.txt" being sent to UUT(alu_top_level.vhd)
-				reg_b         => tb_reg_b,										--reg_b data from "alu_command_file.txt" being sent to UUT(alu_top_level.vhd)
-				 alu_output    => tb_alu_output,							--Operation output from UUT(alu_top_level.vhd) to be displayed on screen by Test Bench
-								
+				clk               => tb_clk,										--clock driving from "alu_top_level_tb.vhd" to UUT(alu_top_level.vhd)
+				alu_output    	  => tb_alu_output,							--Operation output from UUT(alu_top_level.vhd) to be displayed on screen by Test Bench
 				instr_ram_data_in => tb_instr_ram_data_in,
 				instr_ram_addr    => tb_instr_ram_addr,
 				instr_ram_we      => tb_dp_instr_ram_we
@@ -96,66 +101,280 @@ begin
         while now < sim_time_limit loop
             clock_internal <= not clock_internal;
             tb_clk <= clock_internal;  -- Output the clock signal
-
             wait for clock_period / 2;  -- Half the period for 50% duty cycle
         end loop;
-        wait;
+        wait;--required wait statement
     end process;
-	
-	
-  ---------------------------------------
+ 
+
+   tb_instr_ram_addr <= stb_instr_ram_addr;
+	tb_dp_instr_ram_we  <= stb_dp_instr_ram_we;
+  --------------------------------------
   -- Logic
   ---------------------------------------
   
   Proc_Alu_Run_Test : process is
+  
     file test_input_file : text open read_mode is g_command_file;
     variable v_file_line : line;
-
     variable v_read_data : std_logic_vector(7 downto 0); 	
-	
-	
+		
   ---------------------------------------------		
-  -- Procedures		
+  -- Procedures	for TestBench	
   ---------------------------------------------	
   procedure parse_command(variable this_line : line) is 
-	 
-	
-	variable v_cmd      : string(1 to 32);
-	variable v_arg1      : string(1 to 32);	
-	variable v_arg2      : string(1 to 32);	
-	variable v_cmd_len      : natural := 1;
+	variable v_cmd           : string(1 to 32);
+	variable v_arg1          : string(1 to 32);	
+	variable v_arg2          : string(1 to 32);	
+	variable v_cmd_len       : natural := 1;
 	variable v_arg1_len      : natural := 1;
 	variable v_arg2_len      : integer := 1;
+	variable dp_LineStart    : integer := 31;
+	variable dp_LineEnd     : integer := 24;
   
 	begin 
 	
-		sread(v_file_line, v_cmd, v_cmd_len);
-		if v_cmd_len > 0 then
-			if v_cmd(1) /= '#' then 
-				if v_cmd(1 to v_cmd_len) = "STR" then 
-					sread(v_file_line, v_arg1, v_arg1_len);
-					sread(v_file_line, v_arg2, v_arg2_len);
+		sread(v_file_line, v_cmd, v_cmd_len); --Read first chunk of text in line
+		
+		if v_cmd_len > 0 then --did we get a chunk?
+			if v_cmd(1) /= '#' then --make sure chunk not a comment
+				if  v_cmd(1 to v_cmd_len) = "STR"  then --Now we can check command
+					address_tracker <= address_tracker - 8; -- 31-8 = 23
+					clk_wait(tb_clk, 1);
+					if address_tracker = 0 then       --ckeck if we have space to write--if not iterate address by 1
+						stb_instr_ram_addr <= stb_instr_ram_addr + 1;
+						address_tracker <= 31;
+					end if;
+					--write in STR command
+					stb_dp_instr_ram_we <= '1'; --enable write
+					tb_instr_ram_data_in(dp_LineStart downto dp_LineEnd)  <= x"07"; 
+					clk_wait(tb_clk, 1);
+					stb_dp_instr_ram_we <= '0'; --disable write
+					clk_wait(tb_clk, 1);
+					
+					--grab first str argument
+					sread(v_file_line, v_arg1, v_arg1_len); --grab the first argument
+					address_tracker <= address_tracker - 23; --account for amt we will write --should == 0 here
+					clk_wait(tb_clk, 1);
+					
+					if address_tracker = 0 then --ckeck if we have space to write
+							dp_LineStart := 23; --yes we have space so update bit address's
+							dp_LineEnd   := 0;
+							
+							tb_instr_ram_data_in(dp_LineStart downto dp_LineEnd)  <= hstring2slv(v_arg1(1 to v_arg1_len)); --write in ar1
+							stb_dp_instr_ram_we <= '1';
+							clk_wait(tb_clk, 1); --tick in write
+							stb_dp_instr_ram_we <= '0';
+							clk_wait(tb_clk, 1);
+							
+							stb_instr_ram_addr <= stb_instr_ram_addr + 1; 
+							address_tracker <= 31;
+							clk_wait(tb_clk, 1); 
+					end if;
+					
+					--grab second str argument
+					dp_LineStart := 31;
+					dp_LineEnd   := 0;
+					sread(v_file_line, v_arg2, v_arg2_len); --grab the second argument
+					stb_dp_instr_ram_we <= '1';
+					tb_instr_ram_data_in(dp_LineStart downto dp_LineEnd) <= hstring2slv(v_arg2(1 to v_arg2_len)) ;
+					clk_wait(tb_clk, 1); --write the data in
+					stb_dp_instr_ram_we <= '0';
+					stb_instr_ram_addr <= stb_instr_ram_addr + 1; 
+					clk_wait(tb_clk, 1);
+					
+				
+				
+				-----LDA----
+				elsif  v_cmd(1 to v_cmd_len) = "LDA"  then --Now we can check command
+					address_tracker <= address_tracker - 8;
+					clk_wait(tb_clk, 1);
+					if address_tracker = 0 then       --ckeck if we have space to write--if not iterate address by 1
+						stb_instr_ram_addr <= stb_instr_ram_addr + 1;
+						address_tracker <= 31;
+					end if;
+					--write in LDA command
+					stb_dp_instr_ram_we <= '1'; --enable write
+					tb_instr_ram_data_in(dp_LineStart downto dp_LineEnd)  <= x"05"; 
+					clk_wait(tb_clk, 1);
+					stb_dp_instr_ram_we <= '0'; --disable write
+					clk_wait(tb_clk, 1);
+					
+					--grab only LDA argument
+					sread(v_file_line, v_arg1, v_arg1_len); --grab the first argument
+					address_tracker <= address_tracker - 24; --account for amt we will write
+					clk_wait(tb_clk, 1);
+					
+					if address_tracker = 0 then --ckeck if we have space to write "000000"
+							dp_LineStart := 23; --yes we have space so update bit address's
+							dp_LineEnd   := 0;
+							
+							tb_instr_ram_data_in(dp_LineStart downto dp_LineEnd)  <= hstring2slv(v_arg1(1 to v_arg1_len)); --write in ar1
+							stb_dp_instr_ram_we <= '1';
+							clk_wait(tb_clk, 1); --tick in write
+							stb_dp_instr_ram_we <= '0';
+							clk_wait(tb_clk, 1);
+							
+							stb_instr_ram_addr <= stb_instr_ram_addr + 1; 
+							address_tracker <= 31;
+							clk_wait(tb_clk, 1); 
+					end if;
+					dp_LineStart := 23; --yes we have space so update bit address's
+					dp_LineEnd   := 0;
+					clk_wait(tb_clk, 1); --tick in write
+					--write 
+					tb_instr_ram_data_in(dp_LineStart downto dp_LineEnd)  <= hstring2slv(v_arg1(1 to v_arg1_len)); --write in ar1
+					stb_dp_instr_ram_we <= '1';
+					clk_wait(tb_clk, 1); --tick in write
+					stb_dp_instr_ram_we <= '0';
+					clk_wait(tb_clk, 1);
+				
+				-----LDB----
+			elsif  v_cmd(1 to v_cmd_len) = "LDB"  then --Now we can check command
+					address_tracker <= address_tracker - 8;
+					clk_wait(tb_clk, 1);
+					if address_tracker = 0 then       --ckeck if we have space to write--if not iterate address by 1
+						stb_instr_ram_addr <= stb_instr_ram_addr + 1;
+						address_tracker <= 31;
+					end if;
+					--write in LDB command
+					stb_dp_instr_ram_we <= '1'; --enable write
+					tb_instr_ram_data_in(dp_LineStart downto dp_LineEnd)  <= x"06"; 
+					clk_wait(tb_clk, 1);
+					stb_dp_instr_ram_we <= '0'; --disable write
+					clk_wait(tb_clk, 1);
+					
+					--grab only LDB argument
+					sread(v_file_line, v_arg1, v_arg1_len); --grab the first argument
+					address_tracker <= address_tracker - 23; --account for amt we will write
+					clk_wait(tb_clk, 1);
+					
+					if address_tracker = 0 then --ckeck if we have space to write 
+							dp_LineStart := 23; --yes we have space so update bit address's
+							dp_LineEnd   := 0;
+							
+							tb_instr_ram_data_in(dp_LineStart downto dp_LineEnd)  <= hstring2slv(v_arg1(1 to v_arg1_len)); --write in ar1
+							stb_dp_instr_ram_we <= '1';
+							clk_wait(tb_clk, 1); --tick in write
+							stb_dp_instr_ram_we <= '0';
+							clk_wait(tb_clk, 1);
+							
+							stb_instr_ram_addr <= stb_instr_ram_addr + 1; 
+							address_tracker <= 31;
+							clk_wait(tb_clk, 1); 
+					end if;
+					
+					--write 
+					tb_instr_ram_data_in(dp_LineStart downto dp_LineEnd)  <= hstring2slv(v_arg1(1 to v_arg1_len)); --write in ar1
+					stb_dp_instr_ram_we <= '1';
+					clk_wait(tb_clk, 1); --tick in write
+					stb_dp_instr_ram_we <= '0';
+					clk_wait(tb_clk, 1);
+				
+			elsif  v_cmd(1 to v_cmd_len) = "ADD"  then --Now we can check command
+					address_tracker <= address_tracker - 8;
+					clk_wait(tb_clk, 1);
+					if address_tracker = 0 then       --ckeck if we have space to write--if not iterate address by 1
+						stb_instr_ram_addr <= stb_instr_ram_addr + 1;
+						address_tracker <= 31;
+					end if;
+										
+					stb_dp_instr_ram_we <= '1'; --enable write
+					tb_instr_ram_data_in(dp_LineStart downto dp_LineEnd)  <= x"01"; 
+					dp_LineStart := dp_LineStart -8;
+					dp_LineEnd   := dp_LineEnd -8;
+					clk_wait(tb_clk, 1);
+					stb_dp_instr_ram_we <= '0'; --disable write
+					clk_wait(tb_clk, 1);
+					
+					
+				
+			elsif  v_cmd(1 to v_cmd_len) = "RES"  then --Now we can check command--0x0C
+					address_tracker <= address_tracker - 8;
+					clk_wait(tb_clk, 1);
+					--check if we need to iterate dp address
+					if address_tracker = 0 then       --ckeck if we have space to write--if not iterate address by 1
+						stb_instr_ram_addr <= stb_instr_ram_addr + 1;
+						address_tracker <= 31;
+					end if;
+					--write in RES command
+					stb_dp_instr_ram_we <= '1'; --enable write
+					
+					--debug
+					dp_LineStart := dp_LineStart -8;
+					dp_LineEnd   := dp_LineEnd -8;
+					
+					tb_instr_ram_data_in(dp_LineStart downto dp_LineEnd)  <= x"0C"; 
+					clk_wait(tb_clk, 1);
+					stb_dp_instr_ram_we <= '0'; --disable write
+					clk_wait(tb_clk, 1);
+				
+					--grab only RES argument
+					sread(v_file_line, v_arg1, v_arg1_len); --grab the first argument
+					address_tracker <= address_tracker - 8; --account for amt we will write
+					clk_wait(tb_clk, 1);
+					
+					if address_tracker >= 23 then --ckeck if we have space to write 
+							dp_LineStart := 23; --yes we have space so update bit address's
+							dp_LineEnd   := 0;
+							
+							tb_instr_ram_data_in(dp_LineStart downto dp_LineEnd)  <= hstring2slv(v_arg1(1 to v_arg1_len)); --write in ar1
+							stb_dp_instr_ram_we <= '1';
+							clk_wait(tb_clk, 1); --tick in write
+							stb_dp_instr_ram_we <= '0';
+							clk_wait(tb_clk, 1);
+							
+							stb_instr_ram_addr <= stb_instr_ram_addr + 1; 
+							address_tracker <= 31;
+							clk_wait(tb_clk, 1); 
+					end if;
+					
+					--write 
+					address_tracker <= 8;
+					dp_LineStart := 31; --yes we have space so update bit address's
+					dp_LineEnd   := 8;
+					stb_instr_ram_addr <= stb_instr_ram_addr + 1;
+					tb_instr_ram_data_in(dp_LineStart downto dp_LineEnd)  <= hstring2slv(v_arg1(1 to v_arg1_len)); --write in ar1
+					stb_dp_instr_ram_we <= '1';
+					clk_wait(tb_clk, 1); --tick in write
+					stb_dp_instr_ram_we <= '0';
+					clk_wait(tb_clk, 1);
+				
+				
+				--stp
+				elsif  v_cmd(1 to v_cmd_len) = "STP"  then --Now we can check command
+					address_tracker <= address_tracker - 8;
+					dp_LineStart := 7; --yes we have space so update bit address's
+					dp_LineEnd   := 0;
+					clk_wait(tb_clk, 1);
+					--check if we need to iterate dp address
+					if address_tracker = 0 then       --ckeck if we have space to write--if not iterate address by 1
+						stb_instr_ram_addr <= stb_instr_ram_addr + 1;
+						address_tracker <= 31;
+					end if;
+					--write in RES command
+					stb_dp_instr_ram_we <= '1'; --enable write
+					tb_instr_ram_data_in(dp_LineStart downto dp_LineEnd)  <= x"FF"; 
+					clk_wait(tb_clk, 1);
+					stb_dp_instr_ram_we <= '0'; --disable write
+					clk_wait(tb_clk, 1);
+							
 				end if;
-			end if;
-			 print("MADE IT TO THE PARSE COMMAND");
+			 end if;
 		end if;
-	 
 	end procedure parse_command;
-	
+
+
  
 -----------------------------------------------------------------  
 -- Testbench
 -----------------------------------------------------------------  
-  	
-	  
-
-
-		begin  --Test Bench Here
+  		begin  --Test Bench Here
 		wait for 1 fs;
 		while not endfile(test_input_file) loop
- 			readline(test_input_file, v_file_line);                        --read the first line of the file "assembly_instr_input.txt"
+ 			readline(test_input_file, v_file_line);                        
 			parse_command(v_file_line);
-			end loop;											     -- so we can use the read data
+			end loop;											     
 																	 
 		print(cr & "===> End of File Reached");
 		
@@ -166,3 +385,4 @@ begin
 
 
 end architecture TB;
+ 
--- alu_top_level.vhd
+++ alu_top_level.vhd
@@ -1,60 +1,86 @@
-
 library IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
-USE IEEE.STD_LOGIC_MISC.ALL;
-USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+
 entity ALU_1 is
 Generic (
-         g_reg_size    : integer := 4;           -- Not Used. Will leave in here for now in case needed 
-         g_num_of_clks : integer := 7;           -- Just a default number for now. May be changed as seen fit
-         g_operation   : string  := "TEST VALUE" -- Input for Addition, Subtraction, Multiplication, or Division
+         g_REG_SIZE      : integer := 8;           -- Not Used. Will leave in here for now in case needed 
+         g_operation     : string  := "ADD"; -- Input for Addition, Subtraction, Multiplication, or Division
+		 g_RAM_ADDR_SIZE : natural := 6;
+		 g_RAM_DATA_SIZE : natural := 32
         );
 Port(   
-     clk     : in  std_logic; --clock signal
-     reg_a    : in  std_logic_vector(7 downto 0);   -- Input register A
-     reg_b    : in  std_logic_vector(7 downto 0);   -- Input register B
-     alu_output  : out std_logic_vector(7 downto 0);    -- output of ALU
-	 
-	 instr_ram_data_in : in std_logic_vector(32  - 1 downto 0) := (others => '0');
-	 instr_ram_addr    : in unsigned(6 - 1 downto 0) := (others => '0');
-	 instr_ram_we        : in std_logic := '0'
+     clk                 : in  std_logic; --clock signal
+     alu_output          : out std_logic_vector(7 downto 0);    -- output of ALU
+	 instr_ram_data_in   : in std_logic_vector(g_RAM_DATA_SIZE  - 1 downto 0)  := (others => '0');
+	 instr_ram_addr      : in unsigned(g_RAM_ADDR_SIZE - 1 downto 0)          := (others => '0');
+	 instr_ram_we        : in std_logic                                        := '0'
 		);
-     
-end ALU_1;
+end entity ALU_1;
 
 architecture Behavioral of ALU_1 is
 
--------------------------- Signal Declarations --------------------------
-signal s_reg_a   : std_logic_vector(7 downto 0); 
-signal s_reg_b   : std_logic_vector(7 downto 0); 
-signal s_alu_output : std_logic_vector(7 downto 0); 
 
+
+-------------------------- Signal Declarations --------------------------
+signal s_reg_a         : std_logic_vector(g_REG_SIZE - 1 downto 0) := x"01"; 
+signal s_reg_b         : std_logic_vector(g_REG_SIZE - 1 downto 0) := x"02"; 
+signal s_alu_output    : std_logic_vector(7 downto 0)              := x"00";
+
+--RAM Signals 
+--signal instr_ram_we : std_logic;
+--signal instr_ram_data_in  : std_logic_vector(g_ram_data_size - 1 downto 0);
+signal instr_ram_data_out : string(1 to 3) := "000";
+--signal instr_ram_addr     : unsigned(g_ram_addr_size - 1 downto 0);
+
+--RAM Signals
+signal data_ram_we : std_logic;
+signal data_ram_data_in : std_logic_vector(g_RAM_DATA_SIZE - 1 downto 0);
+signal data_ram_data_out : std_logic_vector(g_RAM_DATA_SIZE - 1 downto 0);
+signal data_ram_addr : unsigned(g_RAM_ADDR_SIZE -1 downto 0);
+
+		
+	
 
 BEGIN
-
+ --Instantiate DUAL_PORT_RAM
+ Command_Ram : entity work.dp_ram
+	generic map(
+		simulation  => false,
+		output_regs => false,
+		addr_width => g_ram_addr_size,
+		data_width => g_ram_data_size)
+	port map (
+		p1_clk => clk,
+		p1_we  => instr_ram_we,
+		p1_addr => instr_ram_addr,
+		p1_din => instr_ram_data_in,
+		p1_dout => open,
+		
+		p2_clk => clk,
+		p2_we => '0',
+		p2_addr => data_ram_addr,
+		p2_din => (others => '0'),
+		p2_dout => data_ram_data_out);
+		
 ------------------------- Process Section -------------------------
-process(clk)
-begin
-if(rising_edge(clk)) then 
-    if g_operation = "ADD" then
-        s_alu_output <= s_reg_a + s_reg_b;     -- Addition Operation
-    elsif g_operation = "Subtract" then
-        s_alu_output <= s_reg_a - s_reg_b;     -- Subtraction Operation
-    elsif g_operation = "Multiply" then
-       -- s_alu_output <= s_reg_a * s_reg_b;     -- Multiplication Operation
-    elsif g_operation = "Divide" then
-       -- s_alu_output <= s_reg_a / s_reg_b;     -- Division
-    else 
-        s_alu_output <= s_reg_a + s_reg_b;     -- Default is to perform Addition Operation
-    end if;      
-end if;
-end process;   
-
-
-------------------------- Logic Section -------------------------
-s_reg_a <= reg_a;
-s_reg_b <= reg_b;
-alu_output <= s_alu_output;
+	process(clk)
+		begin
+			if(rising_edge(clk)) then 
+				if  g_operation = "ADD" then
+					s_alu_output <= (s_reg_a) or (s_reg_b);     
+				elsif g_operation = "Subtract" then
+					s_alu_output <= (s_reg_a) and (s_reg_b);    
+				elsif g_operation = "Multiply" then
+					s_alu_output <= (s_reg_a) nor (s_reg_b);    
+				elsif g_operation = "Divide" then
+					s_alu_output <= (s_reg_a) nand (s_reg_b);      
+				else 
+					s_alu_output <= s_reg_a or s_reg_b;   
+				end if;      
+			end if;
+		alu_output <= s_alu_output;
+	end process;   
 
 end Behavioral;
